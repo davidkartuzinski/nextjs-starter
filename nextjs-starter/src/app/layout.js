bcc8acec1de8178a7ffa75f688a160ea03ec387c
davidkartuzinski <david@kaidawei.me>
2025-04-23 16:04:25 +0200

    added a note about the font

--- nextjs-starter/src/app/layout.js
+++ nextjs-starter/src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from '@/contexts/auth-context';
 
+// change the font here.
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
