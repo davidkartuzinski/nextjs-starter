719bc0609119f683eac96d5e7b75f56707422440
davidkartuzinski <david@kaidawei.me>
2025-06-30 20:03:19 +0200

    removed conflict in next config

--- nextjs-starter/next.config.mjs
+++ nextjs-starter/next.config.mjs
@@ -1,5 +1,3 @@
-import createMDX from '@next/mdx';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
@@ -14,9 +12,4 @@ const nextConfig = {
   // Optionally, add any other Next.js config below
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
